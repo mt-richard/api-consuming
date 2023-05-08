5cf0eb7205846625023d14916ff11c85e288f33f
mt-richard <Richard@festimoltd.com>
2023-05-08 12:08:14 +0200

    Final login

---
+++ controller/login.js
@@ -0,0 +1,21 @@
+const {students,admin} = require('../models/index.js')
+
+module.exports= async function login(req, res){
+    const { email, password } = req.body
+    
+    
+    try {
+        const user = await students.findOne({  where: { email: email,password : password} })
+       
+            if(user){
+              res.json({ message: 'Login successful...at master.' });
+            }else{
+            return res.status(401).json({ message: 'Invalid email or password' });
+            }
+
+        
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+      }
+}
